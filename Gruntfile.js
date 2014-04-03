8733fcf079e0d94dce403b446c9fff0bf10ba381
Ben Tedder <ben.tedder@lonelyplanet.com>
2014-04-03 12:59:20 -0500

    added bower, some specs, and a sample test

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,21 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
 
-    pkg: grunt.file.readJSON("package.json")
+    pkg: grunt.file.readJSON("package.json"),
+
+    jasmine: {
+      src: "src/js/*.js",
+      options: {
+        specs: "spec/js/*.js",
+        vendor: "bower_components/jquery/dist/jquery.js"
+      }
+    }
 
   });
+
+  // This loads in all the grunt tasks auto-magically.
+  require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+
+  grunt.registerTask("default", ["jasmine"]);
 
 };
